c63568a53cd33445b4291174a0b7459466cb46f0
nelsonmahesani <nelsonmahesani4@gmail.com>
2026-05-06 10:47:03 +0500

    Add useEffect hook for fetching  data .....

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -51,53 +51,99 @@
 
 
 // --> useState functional component
-import React, { useReducer } from 'react'
-import { useState } from 'react'
-function App() { 
-  const myStyle = {
-        backgroundColor : "green",
-        color : "white",
-        padding : "10px",
-        borderRadius : "5px",
-        border : "none",
-        cursor : "pointer"
-       }
 
-       const [myName, setMyName] = useState("Nelson");
-       function handleChange(e){
-        setMyName(e.target.value)
-        console.log("Change ", e.target.value)
-       }
-       function reducer(action, count){
-        if(action === "increment"){
-          return count + 1
-        }
-        else if(action === "decrement"){
-          return count - 1
-        }
-        else{
-          return count
-        }
-       }
+// import React, { useReducer } from 'react'
+// import { useState } from 'react'
+// function App() { 
+//   const myStyle = {
+//         backgroundColor : "green",
+//         color : "white",
+//         padding : "10px",
+//         borderRadius : "5px",
+//         border : "none",
+//         cursor : "pointer"
+//        }
 
-       const [count, setCount] = useState(0);
-       const[counter, dispatch] = useReducer(reducer,0)
+//        const [myName, setMyName] = useState("Nelson");
+//        function handleChange(e){
+//         setMyName(e.target.value)
+//         console.log("Change ", e.target.value)
+//        }
+//        function reducer(action, count){
+//         if(action === "increment"){
+//           return count + 1
+//         }
+//         else if(action === "decrement"){
+//           return count - 1
+//         }
+//         else{
+//           return count
+//         }
+//        }
+
+//        const [count, setCount] = useState(0);
+//        const[counter, dispatch] = useReducer(reducer,0)
+//   return (
+//     <div>
+//       <h1>{myName}</h1>
+//       <br />
+//       <h1>{counter : count+1}</h1>
+//       <br />
+//       <button onClick={()=>dispatch({type: "increment"})} style={myStyle}>Increment</button>
+//       <button onClick={()=>dispatch({type: "decrement"})} style={myStyle}>Decrement</button>
+
+//       <button
+//         onClick={()=>setMyName(myName +"Mahesani")}
+//         style={myStyle}
+//       >Update</button>
+//       <br />
+//       <input type="text" placeholder='Enter Name' onChange={(e)=>handleChange(e)} />
+//     </div>
+//   )
+// }
+// export default App
+
+//useState--> update the code in return and alson render auto
+// useEffect --> Handle side effects (API calls, event listeners, DOM updates)
+
+// useEffect
+import React, { useEffect, useState } from 'react'
+
+
+function App() {
+  const buttonStyle = {
+    backgroundColor : "blue",
+    color : "white",
+    padding : "10px",
+    borderRadius : "5px",
+    border : "none",
+    cursor : "pointer"
+  }
+  const [age,setAge] = useState(2);
+  const [doubleAge,setDoubleAge] = useState(2);
+
+useEffect(()=>{ 
+  setTimeout(()=>{
+
+setDoubleAge((doubleAge)=>doubleAge * 2)
+},2000)
+},[age]) // dependency array
   return (
     <div>
-      <h1>{myName}</h1>
-      <br />
-      <h1>{counter : count+1}</h1>
-      <br />
-      <button onClick={()=>dispatch({type: "increment"})} style={myStyle}>Increment</button>
-      <button onClick={()=>dispatch({type: "decrement"})} style={myStyle}>Decrement</button>
+        <h1>Use Effect</h1>
+        <p>My Age is : {age}</p>
 
-      <button
-        onClick={()=>setMyName(myName +"Mahesani")}
-        style={myStyle}
-      >Update</button>
-      <br />
-      <input type="text" placeholder='Enter Name' onChange={(e)=>handleChange(e)} />
+        <br />
+        <h1>Double Age: {doubleAge}</h1>
+
+        <button onClick={()=>setAge(age + 1)}
+          style={buttonStyle}
+        >
+          Update Age
+        </button>
+        
     </div>
   )
 }
+
 export default App
