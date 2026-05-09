65614796096e2a4be1842d168b6468528967b9f7
nelsonmahesani <nelsonmahesani4@gmail.com>
2026-05-09 14:47:52 +0500

    login component added

--- react-app/src/route/Routing.jsx
+++ react-app/src/route/Routing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { HashRouter  , Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';    
 import ProductDetail from '../pages/ProductDetail';
@@ -8,14 +8,14 @@ function Routing() {
   return (
     <div>
          
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/products/:title" element={<ProductDetail/>}/>
                 <Route path="/Login" element={<Login/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
 
     </div>
